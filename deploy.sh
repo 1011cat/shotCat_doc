b5bcf3b19d2f2686d14a32d03a887363f683b678
shotCat <bupabuku@gmail.com>
2019-07-14 15:12:15 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,22 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.yourwebsite.com' > CNAME
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果你想要部署到 https://USERNAME.github.io
+# 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:1011cat/1011cat.github.io.git master
 
-# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
